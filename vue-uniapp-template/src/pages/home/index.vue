17a8474589bc9e6902bd08a4a599780c7d49ab00
moxunjingmu <1786471868@qq.com>
2025-03-24 00:52:57 +0800

    feat: 添加首页和我的

---
+++ vue-uniapp-template/src/pages/home/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <view class="flex-center flex-col">
+    <view>
+      <text class="text-cyan font-bold text-lg">首页</text>
+    </view>
+  </view>
+</template>
---
+++ vue-uniapp-template/src/pages/my/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <view class="flex-center flex-col">
+    <view>
+      <text class="text-blue font-bold text-lg">我的</text>
+    </view>
+  </view>
+</template>

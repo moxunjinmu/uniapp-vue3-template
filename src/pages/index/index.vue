9980fff23b8a50297116e81050742769736f22d4
moxunjinmu <1786471868@qq.com>
2025-03-26 23:08:26 +0800

    feat(uviewPlus): :sparkles: 新增uview-Plus组件库

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,16 @@
+<!--
+ * @Author: moxunjinmu
+ * @Date: 2025-03-24 17:39:53
+ * @Description:
+-->
 <template>
   <view class="flex flex-col items-center justify-center p-30rpx">
     <image class="h-200rpx w-200rpx mt-100rpx mx-auto mb-50rpx" src="/static/logo.png" />
     <view class="flex justify-center mb-80rpx">
       <text class="text-36rpx text-gray-500">{{ title }}</text>
+    </view>
+    <view>
+      <u-button type="primary">测试按钮</u-button>
     </view>
     <view class="w-full flex flex-col items-center">
       <text class="text-32rpx font-bold text-gray-800 mb-10rpx">每日心情记录</text>
